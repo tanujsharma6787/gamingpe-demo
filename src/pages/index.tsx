2c485948dec7e20bcbefe8a7b8b02b47a8a5ef25
tanujsharma6787 <tanujsharma6787@gmail.com>
2023-07-26 19:23:41 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
                 </Link>
                 <Link href="#" underline="none">
                     <Button variant='contained'
-                            sx={{...btnStyle,opacity:0.4}}
+                            sx={{...btnStyle, opacity: 0.4}}
                             color='success'
                             size='large'>Payout</Button>
                 </Link>
